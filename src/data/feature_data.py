cb70dca7998342707fc0ff0ca4a3fb76452c1923
B-Deprez <bruno.r.deprez@gmail.com>
2025-10-31 12:57:43 +0100

    bug fix

--- src/data/feature_data.py
+++ src/data/feature_data.py
@@ -1,9 +1,10 @@
+import torch
 from torch.utils.data import Dataset
 
 class ImageDataset(Dataset):
     def __init__(self, X, y, n_channels=1, transform=None):
         self.X = torch.tensor(X, dtype=torch.float32).view(-1, n_channels, 3, 3)
-        self.y = torch.tensor(y, dtype=torch.long)
+        self.y = torch.tensor(y, dtype=torch.float32)
         self.transform = transform
 
     def __len__(self):
