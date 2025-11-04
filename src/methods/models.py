3e9ea143e43ea28c9dfa04e162069d3e0a821726
B-Deprez <bruno.r.deprez@gmail.com>
2025-11-04 17:41:25 +0100

    basic NN models

---
+++ src/methods/models.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size: int, hidden_size: int, output_size: int):
+        super().__init__()
+        #self.flatten = nn.Flatten() # Flattens the 2D image into a 1D array
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, output_size),
+        )
+
+    def forward(self, x):
+        logits = self.linear_relu_stack(x)
+        return logits
+    
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(2, 8, 3, padding=1) 
+        self.conv2 = nn.Conv2d(8, 16, 2)
+        self.fc = nn.Linear(16 * 2 * 2, 1)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = self.fc(x)
+        return x.squeeze()
+    
+class CNN_time(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 8, 3, padding=1) 
+        self.conv2 = nn.Conv2d(8, 16, 3)
+        self.fc = nn.Linear(16 * 6 * 5, 1)
+        self.pool = nn.MaxPool2d(2, 1)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = self.fc(x)
+        return x.squeeze()
