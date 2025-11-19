f5e2d76a919d19142f02d8ee745c5a6d6ff34826
B-Deprez <bruno.r.deprez@gmail.com>
2025-11-19 11:21:28 +0100

    extend processing

--- src/utils/graph_processing.py
+++ src/utils/graph_processing.py
@@ -1,4 +1,54 @@
+import pandas as pd
 import networkx as nx
+
+def graph_degree_rel(G_copy, degree_cutoff=0.01):
+    # Delete the hubs
+    # The cut-off is defined as a relative number
+    
+    degree_df = pd.DataFrame(
+        dict(
+            G_copy.degree()
+        ), 
+        index = ["Degree"]
+    ).transpose()
+
+    degree_threshold = degree_df["Degree"].quantile(1 - degree_cutoff)
+    hub_criteria = degree_df["Degree"] >= degree_threshold
+    
+    hubs_deleted = list(
+                degree_df[hub_criteria].reset_index()["index"]
+            )
+
+    G_copy.remove_nodes_from(
+        hubs_deleted
+    )
+    print(f"Removed {len(hubs_deleted)} hubs with degree >= {degree_threshold}.")   
+    
+    return(G_copy)
+
+def graph_degree_abs(G_copy, degree_cutoff=10): 
+    # Delete the hubs
+    # The cut-off is defined as an absolute number
+    
+    degree_df = pd.DataFrame(
+        dict(
+            G_copy.degree()
+        ), 
+        index = ["Degree"]
+    ).transpose()
+
+    hub_criteria = degree_df["Degree"] >= degree_cutoff
+    
+    hubs_deleted = list(
+                degree_df[hub_criteria].reset_index()["index"]
+            )
+
+    G_copy.remove_nodes_from(
+        hubs_deleted
+    )
+    print(f"Removed {len(hubs_deleted)} hubs with degree >= {degree_cutoff}.")   
+    
+    return(G_copy)
 
 def graph_community(G, resolution=10): # large resolution to have smaller communities
     directed = nx.is_directed(G)
