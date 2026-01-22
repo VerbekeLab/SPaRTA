52ebd243566ba2b8f433d403462a715b6932357a
B-Deprez <bruno.r.deprez@gmail.com>
2026-01-22 16:46:26 +0100

    implementation gpu calculations

--- scripts/experiment_pictures.py
+++ scripts/experiment_pictures.py
@@ -12,22 +12,34 @@ from sklearn.metrics import roc_auc_score
 from src.data.feature_data import NetworkImageDataset
 from src.methods.models import CNN_visual_VGG16
 
-batch_size = 4096
+from tqdm import tqdm
+
+device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"); print(f"Training on: {device}")
+
+batch_size = 512
 
 dataset = NetworkImageDataset()
 loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-model = CNN_visual_VGG16()
+model = CNN_visual_VGG16().to(device)
 
-criterion = torch.nn.BCEWithLogitsLoss()
+y_train = dataset.y.numpy()
+train_weight = round((y_train == 0).sum() / (y_train == 1).sum())*50; print(f"Positive class weight: {train_weight}")
+
+criterion = torch.nn.BCEWithLogitsLoss(pos_weight=torch.tensor([train_weight]).to(device))
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
 
-num_epochs = 100
+print("Starting training...")
+print("Number of batches:", len(loader))
+
+num_epochs = 10
 for epoch in range(num_epochs):
     model.train()
-    for images, labels in loader:
+    for images, labels in tqdm(loader):
+        images = images.to(device, dtype=torch.float32)
+        labels = labels.to(device, dtype=torch.float32)
         optimizer.zero_grad()
-        outputs = model(images.type(torch.float32))
-        loss = criterion(outputs, labels.float().unsqueeze(1))
+        outputs = model(images)
+        loss = criterion(outputs, labels.float())
         loss.backward()
         optimizer.step()
     print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
@@ -37,8 +49,13 @@ for epoch in range(num_epochs):
     y_true = []
     with torch.no_grad():
         for images, labels in loader:
-            outputs = model(images.type(torch.float32))
+            images = images.to(device, dtype=torch.float32)
+            labels = labels.to(device, dtype=torch.float32)
+            outputs = model(images)
             predicted = (torch.sigmoid(outputs) > 0.5).int()
             y_pred.extend(torch.sigmoid(outputs).cpu().numpy().tolist())
             y_true.extend(labels.float().cpu().numpy().tolist())
     print(f'Epoch [{epoch+1}/{num_epochs}], AUC-ROC: {roc_auc_score(y_true, y_pred):.4f}')
+
+# save the trained model
+torch.save(model.state_dict(), 'results/models/cnn_visual_vgg16_experiment_pictures.pth')
